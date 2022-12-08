4b7c647ff050698ae7d461883cc1ee5433692ecf
McCallister Romer <cal.romer@gmail.com>
2022-12-07 22:16:12 -0600

    delete old v2 stuff

--- source/Validation.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Common.h"
-
-int SanitizeFloat(float* value);
-int SanitizeVector(aiVector3D* value);
-int SanitizeVectorArray(aiVector3D* arr, unsigned count);
-int SanitizeMesh(aiMesh* mesh);

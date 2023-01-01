6e7e8617470a8c77bdeee8ef39837f189ef1f6f9
McCallister Romer <cal.romer@gmail.com>
2023-01-01 13:35:56 -0600

    update CubeMap struct; remove legacy converter

--- source/ncconvert/converters/LegacyCubeMapConverter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <filesystem>
-
-namespace nc::convert
-{
-/** @note This exists to retain cubemap support using the old format until textures are implemented. */
-void ConvertCubeMap(const std::filesystem::path& inPath, const std::filesystem::path& outDirectory);
-}

671013c1c4a82c0c191dc72f7bd39c3d83d1a00b
McCallister Romer <cal.romer@gmail.com>
2023-01-16 18:12:32 -0600

    update cmd line options

--- source/ncconvert/Config.h
+++ source/ncconvert/Config.h
@@ -12,9 +12,31 @@ namespace nc::convert
 /** @brief Build controls generated from command line options. */
 struct Config
 {
-    std::optional<std::filesystem::path> singleTargetPath;
-    std::optional<asset::AssetType> singleTargetType;
-    std::optional<std::filesystem::path> targetsFilePath;
-    std::filesystem::path outputDirectory;
+    /** @brief The directory to output .nca files to. */
+    std::filesystem::path outputDirectory = "./";
+
+    /**
+     * @brief A path to a raw asset file to be converted to .nca format.
+     * @note Specific to Mode::Target
+     */
+    std::optional<std::filesystem::path> targetPath;
+
+    /**
+     * @brief The name of the output .nca file.
+     * @note Specific to Mode::Target
+     */
+    std::optional<std::string> assetName;
+
+    /**
+     * @brief The type of the asset to convert.
+     * @note Specific to Mode::Target.
+     */
+    std::optional<asset::AssetType> targetType;
+
+    /**
+     * @brief A path to a manifest containing .nca conversion instructions.
+     * @note Specific to Mode::Manifest.
+     */
+    std::optional<std::filesystem::path> manifestPath;
 };
 } // namespace nc::convert
--- source/ncconvert/Main.cpp
+++ source/ncconvert/Main.cpp
@@ -8,9 +8,50 @@
 #include <iostream>
 #include <string>
 
-const auto DefaultAssetTargetFilename = std::string{"targets.txt"};
+constexpr auto usageMessage = 
+R"(Usage: nc-convert [options]
+Options:
+  -h                      Display this information.
+  -t <asset type>         Specify asset type for a single target.
+  -s <source>             Parse a single asset from <source>.
+  -n <name>               Specify the asset name for a single target.
+  -o <dir>                Output assets to <dir>.
+  -m <manifest>           Perform conversions specified in <manifest>
 
-void Usage();
+Asset types               Supported file types
+  mesh                    fbx, obj
+  hull-collider           fbx, obj
+  concave-collider        fbx, obj
+  texture                 jpg, png, bmp
+  cube-map                jpg, png, bmp
+  audio-clip              wav
+
+Asset names
+  The provided asset name is used to construct the output file path. It may
+  optionally contain the ".nca" file extension and can be prefixed with
+  subdirectories ("meshes/level1/myMesh.nca"). Every asset should always have
+  a unique name.
+
+Json Manifest
+  A provided manifest should be a json file containing an 'assets' array with
+  one or more conversion specifications, and an optional 'globalOptions' object
+  defining global settings. Example:
+  {
+      "globalOptions": {
+          "outputDirectory": "outDir", // default: "./"
+          "workingDirectory": "./"     // default: "./"
+      },
+      "assets": [
+          {
+              "type": "mesh",
+              "sourcePath": "path/to/mesh.fbx",
+              "assetName": "myMesh"
+          }
+      ]
+  }
+
+)";
+
 bool ParseArgs(int argc, char** argv, nc::convert::Config* config);
 
 int main(int argc, char** argv)
@@ -18,7 +59,7 @@ int main(int argc, char** argv)
     auto config = nc::convert::Config{};
     if(!ParseArgs(argc, argv, &config))
     {
-        Usage();
+        std::cout << usageMessage;
         return 1;
     }
 
@@ -32,36 +73,12 @@ int main(int argc, char** argv)
         std::cerr << e.what() << '\n';
         return 1;
     }
+    catch(...)
+    {
+        std::cerr << "Unknown exception\n";
+    }
 
     return 0;
-}
-
-void Usage()
-{
-    std::cout << "Usage: nc-convert [options]\n"
-              << "Options:\n"
-              << "  -h or --help            Display this information\n"
-              << "  -t <target>             Parse a single asset from <target>\n"
-              << "  -a <asset type>         Specify asset type for a single target.\n"
-              << "  -m <manifest>           Parse multiple assets from <manifest>\n"
-              << "  -o <dir>                Output assets to <dir>\n\n"
-              
-              << "  Asset types             Supported file types\n"
-              << "  'mesh'                  fbx, obj\n"
-              << "  'hull-collider          fbx, obj\n"
-              << "  'concave-collider'      fbx, obj\n"
-              << "  'texture'               jpg, png, bmp\n"
-              << "  'cube-map'              jpg, png, bmp\n"
-              << "  'audio-clip'            wav\n\n"
-
-              << "  When using -m, <manifest> should be the path to a newline-separated list of\n"
-              << "  pairs in the form '<asset-type> <path-to-input-file>'.\n\n"
-              
-              << "  If neither -t nor -m are used, the executable's directory will be searched\n"
-              << "  for a manifest named \"targets.txt\".\n\n"
-              
-              << "  All paths should be absolute or relative to the current directory when\n"
-              << "  calling build.exe.\n";
 }
 
 bool ParseArgs(int argc, char** argv, nc::convert::Config* out)
@@ -71,57 +88,57 @@ bool ParseArgs(int argc, char** argv, nc::convert::Config* out)
         return false;
     }
 
-    out->outputDirectory = std::filesystem::path("./");
     auto option = std::string{};
     auto current = 1;
     while (current < argc)
     {
         option = argv[current];
 
-        if(option.compare("-h") == 0 || option.compare("--help") == 0)
+        if (option == "-h")
         {
             return false;
         }
-
-        if(++current >= argc)
+        else if (++current >= argc)
         {
             return false;
         }
-
-        if(option.compare("-t") == 0)
+        else if (option == "-s")
         {
-            out->singleTargetPath = std::filesystem::path(argv[current++]);
-            out->singleTargetPath.value().make_preferred();
-            continue;
+            out->targetPath = std::filesystem::path(argv[current++]);
+            out->targetPath.value().make_preferred();
         }
-
-        if(option.compare("-a") == 0)
+        else if (option == "-n")
         {
-            out->singleTargetType = nc::convert::ToAssetType(std::string{argv[current++]});
-            continue;
+            out->assetName = std::string{argv[current++]};
         }
-
-        if(option.compare("-m") == 0)
+        else if (option == "-t")
         {
-            out->targetsFilePath = std::filesystem::path(argv[current++]);
-            out->targetsFilePath.value().make_preferred();
-            continue;
+            out->targetType = nc::convert::ToAssetType(std::string{argv[current++]});
         }
-
-        if(option.compare("-o") == 0)
+        else if (option == "-m")
+        {
+            out->manifestPath = std::filesystem::path(argv[current++]);
+            out->manifestPath.value().make_preferred();
+        }
+        else if (option == "-o")
         {
             out->outputDirectory = std::filesystem::path(argv[current++]);
             out->outputDirectory.make_preferred();
-            continue;
         }
+        else
+        {
+            return false;
+        }
+    }
 
+    if (out->targetPath && (!out->assetName || !out->targetType))
+    {
         return false;
     }
 
-    if(!out->singleTargetPath && !out->targetsFilePath)
+    if(!out->targetPath && !out->manifestPath)
     {
-        out->targetsFilePath = std::filesystem::path(argv[0]).replace_filename(DefaultAssetTargetFilename);
-        out->targetsFilePath.value().make_preferred();
+        return false;
     }
 
     return true;

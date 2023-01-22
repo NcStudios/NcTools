8d0569b702af74b1510ec367aea487c222402748
McCallister Romer <cal.romer@gmail.com>
2023-01-21 22:14:29 -0600

    split NcAsset headers

---
+++ include/ncasset/AssetType.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace nc::asset
+{
+enum class AssetType
+{
+    AudioClip,
+    CubeMap,
+    ConcaveCollider,
+    HullCollider,
+    Mesh,
+    Shader,
+    Texture
+};
+} // namespace nc::asset

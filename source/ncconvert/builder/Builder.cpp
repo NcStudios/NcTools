f9390f88085a661621f1b0333bea3c058249bfc9
McCallisterRomer <cal.romer@gmail.com>
2024-02-11 20:00:04 -0600

    update version

--- source/ncconvert/builder/Builder.cpp
+++ source/ncconvert/builder/Builder.cpp
@@ -107,12 +107,13 @@ auto Builder::Build(asset::AssetType type, const Target& target) -> bool
             convert::Serialize(outFile, asset, assetId);
             return true;
         }
-        default:
+        case asset::AssetType::Font:
         {
-            throw NcError(fmt::format("Unknown AssetType: {} for {}",
-                static_cast<int>(type), target.sourcePath.string()
-            ));
+            throw NcError("Not implemented");
         }
     }
+    throw NcError(fmt::format("Unknown AssetType: {} for {}",
+        static_cast<int>(type), target.sourcePath.string()
+    ));
 }
 } // namespace nc::convert
